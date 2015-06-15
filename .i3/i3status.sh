07832c018c6160465983bd786fea133f81ac7d59
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2015-06-15 18:53:23 +0200

    i3status linting

--- .i3/i3status.sh
+++ .i3/i3status.sh
@@ -5,20 +5,22 @@
 touch /tmp/last_fm_last_scrobble
 
 function now_playing_spotify() {
+  if pgrep spotify &> /dev/null; then
     xdotool search \
---name "Spotify Premium - Linux Preview" | 
-xargs xprop -id | 
-grep "^_NET_WM_ICON_NAME" | 
-cut -d '=' -f2 | 
-sed -e 's/\(^\s\+\"Spotify\)\(\s\+\-\s\+\)*//' -e 's/\"*$//' 
+--name "Spotify Premium - Linux Preview" |
+xargs xprop -id |
+grep "^_NET_WM_ICON_NAME" |
+cut -d '=' -f2 |
+sed -e 's/\(^\s\+\"Spotify\)\(\s\+\-\s\+\)*//' -e 's/\"*$//'
+  fi
 }
 
 function now_playing_last_fm() {
     #last_fm_np=$(create_element "last_fm_np" "lastfm" "$(now_playing_last_fm)" "#FF00FF")
     track=$(cat /tmp/last_fm_last_scrobble)
-    if test `find "/tmp/last_fm_last_scrobble" -mmin +2`; then
+    if test "$(find "/tmp/last_fm_last_scrobble" -mmin +2)"; then
         track=$(curl -s http://ws.audioscrobbler.com/2.0/user/chauney/recenttracks.rss | xmlstarlet sel -T -t -m '//item[1]/title/text()' -v '.' -n)
-        echo $track > /tmp/last_fm_last_scrobble
+        echo "$track" > /tmp/last_fm_last_scrobble
     fi
     echo "last scrobble : $track"
 }
@@ -31,15 +33,15 @@ function create_element() {
     echo "{\"name\":\"$name\",\"instance\":\"$instance\",\"full_text\":\"$text\",\"color\":\"$color\"}"
 }
 
-i3status --config $1 | (read line && echo $line && read line && echo $line && while :
+i3status --config "$1" | (read line && echo "$line" && read line && echo "$line" && while :
 do
-    read line 
-    startchr=`expr index "$line" C[`
+    read line
+    startchr="$(expr index "$line" C[)"
     prefix=${line:0:${startchr}}
     line="${line:${startchr}:${#line}-${startchr}-1}"
 
     #last_fm_np=$(create_element "last_fm_np" "lastfm" "$(now_playing_last_fm)" "#FF00FF")
     spotify_np=$(create_element "spotify_np" "spotify" "$(now_playing_spotify)" "#FF00FF")
-    echo "$prefix $spotify_np, $line ]" # || exit 1 
-    
+    echo "$prefix $spotify_np, $line ]" # || exit 1
+
 done)
