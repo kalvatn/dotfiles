1f9c1ba0e1d8348307ed44ae774701aa408dc71b
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-07-04 19:40:41 +0200

    suppress errors from wildfly queues

--- scripts/wildfly/read-queues.py
+++ scripts/wildfly/read-queues.py
@@ -9,11 +9,17 @@ def main():
     WILDFLY_HOME = os.environ.get('WILDFLY_HOME')
     cmd = WILDFLY_HOME + r'/bin/jboss-cli.sh --connect "/subsystem=messaging/hornetq-server=default/:read-children-resources\(child-type=jms-queue,include-runtime=true\)"'
     #print cmd.split()
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    FNULL = open(os.devnull, 'w')
+    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=FNULL)
     out, err = process.communicate()
 
     if err:
-        print err
+        print "error reading queues"
+        return
+
+    if not out:
+        print "error reading queues, is server running?"
+        return
 
     # convert to json
     out = out.replace("=>", ":")
@@ -21,11 +27,15 @@ def main():
     out = re.sub(r': (\w+),', r': "\1",', out)
 
     json_data = json.loads(out)
-    queue_info = json_data['result']
-    print '%30s %15s %15s' % ('queue', 'processing', 'total_added')
-    print '-'*15*5
-    for key, value in queue_info.items():
-        print '%30s %15s %15s' % (key, value['message-count'], value['messages-added'])
+    try:
+        queue_info = json_data['result']
+        print '%30s %15s %15s' % ('queue', 'processing', 'total_added')
+        print '-'*15*5
+        for key, value in queue_info.items():
+            print '%30s %15s %15s' % (key, value['message-count'], value['messages-added'])
+    except KeyError:
+        print "queues not initialized"
+        pass
 
 if __name__ == '__main__':
     main()
