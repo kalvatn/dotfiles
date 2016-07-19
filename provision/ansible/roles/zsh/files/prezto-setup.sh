f54cd4d5c69ae99aa8fca8c015741997f03b3768
Jon Terje Tvergrov Kalvatn <jonterje.kalvatn@gmail.com>
2016-07-19 21:43:11 +0200

    zsh: prezto task

---
+++ provision/ansible/roles/zsh/files/prezto-setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+
+set -e
+
+# see https://github.com/sorin-ionescu/prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+	ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
